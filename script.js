c8a3a742cf06930c1a9bfced411c34fefba70090
Isaias <figueiredo.isaias@gmail.com>
2022-08-22 12:54:00 -0300

    second version

--- script.js
+++ script.js
@@ -1,6 +1,34 @@
+const cards = ['unicornparrot', 'unicornparrot', 'tripletsparrot', 'tripletsparrot', 'revertitparrot', 'revertitparrot', 'metalparrot', 'metalparrot', 'fiestaparrot', 'fiestaparrot', 'explodyparrot', 'explodyparrot', 'bobrossparrot', 'bobrossparrot']
 let hasflippedcard=false
 let firstcard,secondcard;
 let avoidbug =false // evita o bug de virar várias cartas enquanto o settimeout não executa
+let novoarranjo=''
+let m=document.querySelectorAll('.single-card')
+console.log(m[1])
+let lista=[m[0].dataset.image,m[1].dataset.image,m[2].dataset.image,m[3].dataset.image,m[4].dataset.image,m[5].dataset.image,m[6].dataset.image,m[7].dataset.image,m[8].dataset.image,m[9].dataset.image,m[10].dataset.image,m[11].dataset.image,m[12].dataset.image,m[13].dataset.image]
+console.log(lista)
+
+
+function howmanycards(){
+    let play=Number(prompt('Com quantas cartas deseja jogar?'))
+    let avaliblecards= [4,6,8,10,12,14]
+    while(!avaliblecards.includes(play)){
+            play=Number(prompt('Com quantas cartas deseja jogar?'))       
+    }
+    let arranjo = lista.slice(0,play);
+    arranjo.sort(() => Math.random() - 0.5);
+    console.log(arranjo)
+    for(let i=0;i<arranjo.length;i++){
+        novoarranjo+=`<div class="single-card" data-image="${arranjo[i]}" onclick="clicked(this)">
+             <img class="bird" src="images/front.png" alt="passaro">
+             <img class="front-card" src="images/${arranjo[i]}parrot.gif" alt="Parrot">
+        </div>`
+        document.querySelector('.cards').innerHTML=novoarranjo
+    }
+    console.log(novoarranjo)
+}
+setTimeout(howmanycards,1000)
+
 
 function clicked(x){
     if (avoidbug) return;
@@ -28,7 +56,7 @@ function clicked(x){
         else{
             //se não der match
             avoidbug=true
-                setTimeout(()=>{
+             setTimeout(()=>{
             firstcard.classList.remove('rotate')
             firstcard.childNodes[1].classList.remove('hidden')
             firstcard.childNodes[3].classList.remove('visible')
@@ -42,15 +70,3 @@ function clicked(x){
     }
    
 }
-
-/*function howmanycards(){
-    let play=Number(prompt('Com quantas cartas deseja jogar?'))
-    let avaliblecards= [4,6,8,10,12,14]
-    while(!avaliblecards.includes(play)){
-            play=Number(prompt('Com quantas cartas deseja jogar?'))       
-    }
-}
-setTimeout(howmanycards,1000)
-*/
-
- 
